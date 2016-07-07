0a943a5d8078efbf64f52b372aabd0dcb173e346
74sharlock <2220106810@qq.com>
2016-07-07 16:22:52 +0800

    a

---
+++ app/components/ui/icon.vue
@@ -0,0 +1,26 @@
+<template>
+    <i class="fa" :class="className" :style="{fontSize: !!size ? size + 'px' : ''}"></i>
+</template>
+
+<script type="text/babel">
+    export default {
+        props: ['name', 'type', 'size'],
+        data () {
+            return {
+                
+            }
+        },
+        computed: {
+            className(){
+                let className = '';
+                if(!!this.name){
+                    className += ` fa-${this.name}`;
+                }
+                if(!!this.type){
+                    className += ` fa-${this.type}`;
+                }
+                return className;
+            }
+        }
+    }
+</script>
---
+++ app/pages/member.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+<style lang="less" rel="stylesheet/less" scoped>
+
+</style>
+<script type="text/babel">
+    export default {
+        data () {
+            return {
+                
+            }
+        },
+        components: {}
+    }
+</script>
